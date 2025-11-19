e2854faf629e35d398754cd8871b92f2ab0be009
shuvostack <shuvostack@gmail.com>
2025-11-19 16:41:59 +0600

    Created AuthLayout and router setup for login and register

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -3,6 +3,9 @@ import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/Home/Home/Home";
 import Coverage from "../pages/Coverage/Coverage";
 import Error from "../pages/Error/Error";
+import AuthLayout from "../layouts/AuthLayout";
+import Login from "../pages/Auth/Login/Login";
+import Register from "../pages/Auth/Register/Register";
 
 export const router = createBrowserRouter([
   {
@@ -21,4 +24,18 @@ export const router = createBrowserRouter([
       }
     ]
   },
+  {
+    path: "/",
+    Component: AuthLayout,
+    children: [
+      {
+        path: "login",
+        Component: Login
+      },
+      {
+        path: "register",
+        Component: Register
+      }
+    ]
+  }
 ]);
